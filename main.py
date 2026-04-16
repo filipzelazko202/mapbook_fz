21a8d58ce6622711683c13c955bcf78c4a03d930
filipzelazko202 <271182296+filipzelazko202@users.noreply.github.com>
2026-04-16 14:00:34 +0200

    wip: mvs template applied

--- main.py
+++ main.py
@@ -1,18 +1,6 @@
-users: list = [
-    {'username': 'oliwia', 'location': 'łódź', 'posts': 1,
-     'usermessage': ['życzenia1', 'kocham legie', 'sprzedam opla', 'kiwi']},
-    {'username': 'paweł', 'location': 'ostróda', 'posts': 2,
-     'usermessage': ['życzenia2', 'kocham legie', 'sprzedam opla', ]},
-    {'username': 'eliza', 'location': 'radom', 'posts': 3,
-     'usermessage': ['życzenia3', 'kocham legie', 'sprzedam opla', ]},
-    {'username': 'ewelina', 'location': 'dęblin', 'posts': 4,
-     'usermessage': ['życzenia4', 'kocham legie', 'sprzedam opla', ]},
-]
+from mapbook_lib.model import  users
+from mapbook_lib.controller import read_data
 
 
-def read_data(users_data: list) -> None:
-    for user in users_data:
-        print(
-            f'twój znajomy {user['username']} z miejscowości {user["location"]} opublikował {user['posts']} wiadomości. Ostatnia wiadomość {user['usermessage']}')
 
 read_data(users[1:])
---
+++ mapbook_lib/model.py
@@ -0,0 +1,9 @@
+users: list = [
+    {'username': 'oliwia', 'location': 'łódź', 'posts': 1,
+     'usermessage': ['życzenia1', 'kocham legie', 'sprzedam opla', 'kiwi']},
+    {'username': 'paweł', 'location': 'ostróda', 'posts': 2,
+     'usermessage': ['życzenia2', 'kocham legie', 'sprzedam opla', ]},
+    {'username': 'eliza', 'location': 'radom', 'posts': 3,
+     'usermessage': ['życzenia3', 'kocham legie', 'sprzedam opla', ]},
+    {'username': 'ewelina', 'location': 'dęblin', 'posts': 4,
+     'usermessage': ['życzenia4', 'kocham legie', 'sprzedam opla', ]},]
