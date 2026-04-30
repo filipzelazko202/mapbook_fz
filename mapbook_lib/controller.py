9c6ac8f418f67247d6a73b932d45d33c66fb5a49
filipzelazko202 <271182296+filipzelazko202@users.noreply.github.com>
2026-04-30 12:40:04 +0200

    feat: add insert function

--- mapbook_lib/controller.py
+++ mapbook_lib/controller.py
@@ -2,3 +2,12 @@ def read_data(users_data: list) -> None:
     for user in users_data:
         print(
             f'twój znajomy {user['username']} z miejscowości {user["location"]} opublikował {user['posts']} wiadomości. Ostatnia wiadomość {user['usermessage']}')
+
+
+def add_user(users_data: list) -> None:
+    name = input('Podaj imię: ')
+    place = input('Podaj miejscowość: ')
+    post_num = int(input('Ile masz postów: '))
+    usermessage = ('')
+    mes_info = input('Przekaż wiadomość: ').split()
+    users_data.append({"username": name, "location": place, "posts": post_num, "usermessage": mes_info})
